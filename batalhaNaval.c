f49ee5003c5614d9b65a6df6c4b9ecc340dd8d0c
PdrinX <pedrosantosbenedetti@gmail.com>
2025-03-26 15:44:13 +0000

    Batalha Naval Avançado

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,120 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+#define COLUNAS 5
+#define LINHAS 3
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+void inserirMatriz(int tabuleiro[10][10], int Efeito[LINHAS][COLUNAS], int startRow, int startCol) {
+    for (int i = 0; i < LINHAS; i++) {
+        for (int j = 0; j < COLUNAS; j++) {
+            tabuleiro[startRow + i][startCol + j] = Efeito[i][j];
+        }
+    }
+}
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+//Criando a área de efeito das bombas
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+//Cone
+int coneEfeito[LINHAS][COLUNAS] = {0};
+void cone() {
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+        for(int i = 0; i <= 2; i++){
+            for(int j = 0; j <= 4; j++){
+                coneEfeito[i][2] = 5;
+                coneEfeito[2][j] = 5;
+                if (j >= 1 && i == 1 && j <= 3){
+                    coneEfeito[1][j] = 5;
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+                }
+    
+            }
+        }
+}
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+//Cruz
+int cruzEfeito[LINHAS][COLUNAS] = {0};
+void cruz() {
+        for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 5; j++){
+            cruzEfeito[i][2] = 7;
+            cruzEfeito[1][j] = 7;
+        }
+    }
+}
+
+//Octaedro
+int octaedroEfeito[LINHAS][COLUNAS] = {0};
+void octaedro(){
+    for(int i = 0; i < 3; i++){
+       for(int j = 1; j <5; j++){
+        octaedroEfeito[i][2] = 8;
+        octaedroEfeito[1][j] = 8;
+        if(j == 3){
+            break;
+        }
+       }
+    }
+
+}
+
+
+
+int main(){
+
+    printf("Jogo Batalha Naval !!!\n");
+    printf(" ");
+
+    //Crinado o tabuleiro utilizando matriz
+    int numero[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    char letras[10]= {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+
+    for(int i = 0; i < 10; i++){
+        printf(" %c", letras[i]);//Imprime as letras
+
+    }
+    printf("\n");
+
+   
+    int tabuleiro[10][10] = {0};
+
+    for (int i = 1; i <= 3; i++) {
+        tabuleiro[i][i + 1] = 3;
+        for (int j = 5; j <= 7; j++) {//Imprime os navios horizontal e vertical e um diagonal
+            tabuleiro[5][j] = 3;
+            tabuleiro[i][7] = 3;
+        }
+    }
+
+    int num = 2;
+    for(int i = 7; i <= 9; i++){//Imprime outro diagonal
+
+        tabuleiro[i][num] = 3;
+        num--;
+    }
+
+    //Adicionando o efeito das bombas
+    cone();
+    cruz();
+    octaedro();
+
+    inserirMatriz(tabuleiro, coneEfeito, 4, 4);
+    inserirMatriz(tabuleiro, cruzEfeito, 2, 0);
+    inserirMatriz(tabuleiro, octaedroEfeito, 7, 3);
+
+
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 10; j++){
+
+            if (j == 0) {
+                printf("%i ", numero[i]);//mprime os numeros das colunas
+            }
+
+            printf("%d ", tabuleiro[i][j]);//Imprime os 0's
+            if(i == 3 && j == 4){
+                cone();
+                }
+            }
+        printf("\n");
+    }
 
     return 0;
 }
